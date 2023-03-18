ddfc750a012e47c3e037e1b48b463aec8eb083d6
Pedro Reis <pedro.reis@cejamrj.org.br>
2023-03-18 10:04:23 -0300

    add: function to calculate total hours with service

--- src/screens/RequestPage/index.tsx
+++ src/screens/RequestPage/index.tsx
@@ -21,19 +21,28 @@ const availableHours = [
 		id: 1,
 		day: 'segunda-feira',
 		hour: '7am - 8am',
+		value: 1,
 	},
-
 	{
 		id: 2,
 		day: 'segunda-feira',
 		hour: '11am - 15pm',
+		value: 4,
 	},
 	{
 		id: 3,
 		day: 'sexta-feira',
 		hour: '18pm - 23pm',
+		value: 5,
 	},
 ];
+
+const calculateTotalHours = (items: any) => {
+	return items.reduce((total: any, item: any) => {
+		const hour = availableHours.find((h) => h.id === item);
+		return total + hour.value;
+	}, 0);
+};
 
 export function RequestPage() {
 	const { navigate } = useNavigation();
@@ -75,9 +84,6 @@ export function RequestPage() {
 							Tipo de serviço:
 						</Text>
 						<Text>Caminhada</Text>
-						<Text style={[styles.title, styles.gray]}>
-							Horas/dias disponíveis:
-						</Text>
 						<View>
 							<Text style={[styles.title, styles.gray]}>
 								Horas/dias disponíveis:
@@ -85,10 +91,7 @@ export function RequestPage() {
 							{availableHours.map((item) => (
 								<View
 									key={item.id}
-									style={{
-										flexDirection: 'row',
-										alignItems: 'center',
-									}}
+									style={styles.itemContainer}
 								>
 									<Switch
 										value={checkedItems.includes(item.id)}
@@ -103,10 +106,10 @@ export function RequestPage() {
 											)
 										}
 									/>
-									<Text style={{ marginLeft: 10 }}>
+									<Text style={styles.itemText}>
 										{item.day}: {item.hour}{' '}
 										{checkedItems.includes(item.id) && (
-											<Text style={{ color: 'red' }}>
+											<Text style={styles.reservedText}>
 												Reservada
 											</Text>
 										)}
@@ -120,12 +123,12 @@ export function RequestPage() {
 							will occur in every week.
 						</Text> */}
 
-						<View>
-							<Text style={[styles.title, styles.gray]}>
-								Valor por serviço
+						<Text style={[styles.title, styles.gray]}>
+							Valor total do serviço:{' '}
+							<Text style={{ color: '#40B5A2' }}>
+								R${calculateTotalHours(checkedItems)}
 							</Text>
-							<Text>R$ 12,00/hr</Text>
-						</View>
+						</Text>
 
 						<View style={styles.details}>
 							<View
@@ -145,20 +148,6 @@ export function RequestPage() {
 							</View>
 
 							<View style={styles.buttonGroup}>
-								<TouchableOpacity
-									style={styles.chatButton}
-									activeOpacity={0.6}
-									// onPress={() => navigate('Register' as never)}
-								>
-									<Text style={styles.buttonText}>
-										<Ionicons
-											name="chatbox-outline"
-											size={24}
-											color="#000"
-										/>
-									</Text>
-								</TouchableOpacity>
-
 								<TouchableOpacity
 									style={styles.button}
 									activeOpacity={0.6}
@@ -256,6 +245,14 @@ const styles = StyleSheet.create({
 		gap: 10,
 		marginTop: 20,
 	},
+	button: {
+		backgroundColor: '#40B5A2',
+		width: '100%',
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderRadius: 2,
+		paddingVertical: 15,
+	},
 	chatButton: {
 		width: 60,
 		height: 56,
@@ -264,18 +261,24 @@ const styles = StyleSheet.create({
 		borderColor: '#000',
 		borderWidth: 2,
 	},
-	button: {
-		backgroundColor: '#40B5A2',
-		width: '80%',
-		justifyContent: 'center',
-		alignItems: 'center',
-		borderRadius: 2,
-		paddingVertical: 15,
-	},
 	buttonText: {
 		fontSize: 18,
 		fontWeight: 'bold',
 		color: '#FFFFFF',
 		textTransform: 'uppercase',
+	},
+	itemContainer: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		marginBottom: 10,
+	},
+	itemText: {
+		marginLeft: 10,
+		fontSize: 16,
+		fontWeight: '400',
+	},
+	reservedText: {
+		color: 'red',
+		fontWeight: 'bold',
 	},
 });
